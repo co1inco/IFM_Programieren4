4e8356157257b137e9085147e47c0da46915649a
Colin Meihöfer <colin.meihoefer@gmx.de>
2026-05-02 11:17:26 +0200

    Messing with payara debugging

--- Webanwendungen/demo/src/main/java/com/example/HelloServlet.java
+++ Webanwendungen/demo/src/main/java/com/example/HelloServlet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     resp.setContentType("text/plain");
-    var test = "Hello world ABC";
+    var test = "Hello world ABC DEF";
     // resp.getWriter().println("Hello from Payara!");
     resp.getWriter().println(test);
   }
